93f0fd09987e796b99e0b22fb7081cc6cd6ed531
Jon Winton <winton.jon@gmail.com>
2019-03-13 11:17:17 -0400

    updating file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,14 @@
 set -e
 set -u
 
+# Make sure PROJECT_NAME is set for the siteConfig.js file
+if [ -z ${PROJECT_NAME+x} ]; then
+  echo "PROJECT_NAME is not set, falling back to default"
+  export PROJECT_NAME="site"
+else
+  echo "PROJECT_NAME is set to '$PROJECT_NAME'"
+fi
+
 SSH_PATH="/root/.ssh"
 
 mkdir "$SSH_PATH"
@@ -21,13 +29,13 @@ ssh-keyscan -t rsa "github.com" >> "$SSH_PATH/known_hosts"
 echo "StrictHostKeyChecking no" >> "$SSH_PATH/config"
 
 # Change into the build directory
-cd $BUILD_DIR
+cd $BUILD_DIR;
 
 git config --global user.name "$GITHUB_ACTOR" && \
 git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com" && \
 npm install && \
 npm run build && \
-npx gh-pages -d build/clay-cli --repo "git@github.com:$GITHUB_REPOSITORY.git"
+npx gh-pages -d "build/$PROJECT_NAME" --repo "git@github.com:$GITHUB_REPOSITORY.git"
 # We need for force an SSH connection to use the SSH key so
 # we can specify the `--repo` flag to make sure `gh-pages`
 # uses the SSH url https://github.com/tschaub/gh-pages/issues/160
